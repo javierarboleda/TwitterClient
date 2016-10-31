6836f563608725e0f03709d4ab3addcfe31d9bcd
Javier Arboleda <javier.g.arboleda@gmail.com>
2016-10-30 21:09:21 -0700

    feat: Add Glide for more efficient image rendering!

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsArrayAdapter.java
@@ -6,15 +6,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.databinding.ItemTweetBinding;
 import com.codepath.apps.restclienttemplate.models.Tweet;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.raizlabs.android.dbflow.config.FlowManager.getContext;
 
 /**
  * Created on 10/27/16.
@@ -30,27 +28,6 @@ public class TweetsArrayAdapter extends RecyclerView.Adapter<TweetsArrayAdapter.
         mContext = context;
         mTweets = tweets;
     }
-
-    //@Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        // 1. Get the tweet
-//        Tweet tweet = getItem(position);
-//        // 2. Find or inflate the template
-//        if (convertView == null) {
-//            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
-//        }
-//        // 3. Find the subviews to fill with data in the template
-//        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfilePic);
-//        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
-//        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
-//        // 4. Populate data into the subviews
-//        tvUserName.setText(tweet.getUser().getScreenName());
-//        tvBody.setText(tweet.getBody());
-//        ivProfileImage.setImageResource(android.R.color.transparent); // clear out old image
-//        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(ivProfileImage);
-//        // 5. Return the view to be inserted into the list
-//        return convertView;
-//    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -76,7 +53,7 @@ public class TweetsArrayAdapter extends RecyclerView.Adapter<TweetsArrayAdapter.
         holder.binding.tvScreenName.setText(screenName);
         holder.binding.tvRelativeTime.setText(tweet.getRelativeTimeAgo());
 
-        Picasso.with(getContext())
+        Glide.with(mContext)
                 .load(tweet.getUser().getProfileImageUrl())
                 .into(holder.binding.ivProfilePic);
     }
