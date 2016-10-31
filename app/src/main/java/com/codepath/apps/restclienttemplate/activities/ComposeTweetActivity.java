7ca686c3bd54179c105a81f094f44b3ea1a09e6f
Javier Arboleda <javier.g.arboleda@gmail.com>
2016-10-30 18:40:28 -0700

    feat: Add compose tweet character counter

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeTweetActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeTweetActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 
@@ -28,6 +30,39 @@ public class ComposeTweetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_compose_tweet);
+
+//        binding.tvCharsLeft.setText("100");
+
+        binding.etTweetInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int charsLeft = 140 - editable.length();
+                String charsLeftString = "" + charsLeft;
+                binding.tvCharsLeft.setText(charsLeftString);
+
+                if (charsLeft < 0) {
+                    binding.tvCharsLeft.setTextColor(getResources().getColor(R.color.redText));
+                    binding.btnTweet.setEnabled(false);
+                    binding.btnTweet.setBackground(getResources()
+                            .getDrawable(R.drawable.shape_round_button_disabled));
+                } else {
+                    binding.tvCharsLeft.setTextColor(getResources().getColor(R.color.blackText));
+                    binding.btnTweet.setEnabled(true);
+                    binding.btnTweet.setBackground(getResources()
+                            .getDrawable(R.drawable.shape_round_button));
+                }
+            }
+        });
     }
 
     public void sendTweet(View view) {
